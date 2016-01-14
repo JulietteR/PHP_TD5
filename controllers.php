b245d4c8fa01a7a36fccbff4c22299bfadb14544
Juliette Rivière <juliette@riviere.im>
2016-01-14 22:18:23 +0100

    possibilite demprunt

---
+++ controllers.php
@@ -0,0 +1,100 @@
+<?php
+
+use Gregwar\Image\Image;
+
+$app->match('/', function() use ($app) {
+    return $app['twig']->render('home.html.twig');
+})->bind('home');
+
+$app->match('/books', function() use ($app) {
+    return $app['twig']->render('books.html.twig', array(
+        'books' => $app['model']->getBooks()
+        ));
+})->bind('books');
+
+$app->match('/aBook/{id}', function($id) use ($app) {
+ $book = $app['model']->getABook($id)[0];
+ $exemplaires =  $app['model']->getExemplairesABook($id);
+ return $app['twig']->render('aBook.html.twig', array(
+    'aBook' => $book,
+    'exemplaires' => $exemplaires
+    ));
+})->bind('aBook');
+
+$app->match('/loanABook/{id}', function($id) use ($app) {
+    $request = $app['request'];
+    $success = false;
+    if ($request->getMethod() == 'POST') {
+     $post = $request->request;
+     $endingDateWithTime = $post->get('end_date')."/".date('H/i/s');
+      // get end time and add the hour/minutes/seconds when the book as been rented.
+
+        // Saving the borrow to database
+     $app['model']->borrowABook($id, $post->get('borrower_name'), date('Y/m/d/H/i/s'), $endingDateWithTime   );
+     $success = true;
+
+ }
+ return $app['twig']->render('loanABook.html.twig', array(
+    'success' => $success
+    ));
+
+})->bind('loanABook');
+
+
+
+
+$app->match('/admin', function() use ($app) {
+    $request = $app['request'];
+    $success = false;
+    if ($request->getMethod() == 'POST') {
+        $post = $request->request;
+        if ($post->has('login') && $post->has('password') &&
+            /*array($post->get('login'), $post->get('password')) == */
+            $app['config']['admin'][$post->get('login')] == $post->get('password')) {
+            $app['session']->set('admin', true);
+        $success = true;
+    }
+}
+return $app['twig']->render('admin.html.twig', array(
+    'success' => $success
+    ));
+})->bind('admin');
+
+$app->match('/logout', function() use ($app) {
+    $app['session']->remove('admin');
+    return $app->redirect($app['url_generator']->generate('admin'));
+})->bind('logout');
+
+$app->match('/addBook', function() use ($app) {
+    if (!$app['session']->has('admin')) {
+        return $app['twig']->render('shouldBeAdmin.html.twig');
+    }
+
+    $request = $app['request'];
+    if ($request->getMethod() == 'POST') {
+        $post = $request->request;
+        if ($post->has('title') && $post->has('author') && $post->has('synopsis') &&
+            $post->has('copies')) {
+            $files = $request->files;
+        $image = '';
+
+            // Resizing image
+        if ($files->has('image') && $files->get('image')) {
+            $image = sha1(mt_rand().time());
+            Image::open($files->get('image')->getPathName())
+            ->resize(240, 300)
+            ->save('uploads/'.$image.'.jpg');
+            Image::open($files->get('image')->getPathName())
+            ->resize(120, 150)
+            ->save('uploads/'.$image.'_small.jpg');
+        }
+
+            // Saving the book to database
+        $app['model']->insertBook($post->get('title'), $post->get('author'), $post->get('synopsis'),
+            $image, (int)$post->get('copies'));
+    }
+}
+
+return $app['twig']->render('addBook.html.twig');
+})->bind('addBook');
+
---
+++ src/Model.php
@@ -0,0 +1,121 @@
+<?php
+
+class Model
+{
+    protected $pdo;
+
+    public function __construct(array $config)
+    {
+        try {
+            if ($config['engine'] == 'mysql') {
+                $this->pdo = new \PDO(
+                    'mysql:dbname='.$config['database'].';host='.$config['host'],
+                    $config['user'],
+                    $config['password']
+                );
+                $this->pdo->exec('SET CHARSET UTF8');
+            } else {
+                $this->pdo = new \PDO(
+                    'sqlite:'.$config['file']
+                );
+            }
+        } catch (\PDOException $error) {
+            throw new ModelException('Unable to connect to database');
+        }
+    }
+
+    /**
+     * Tries to execute a statement, throw an explicit exception on failure
+     */
+    protected function execute(\PDOStatement $query, array $variables = array())
+    {
+        if (!$query->execute($variables)) {
+            $errors = $query->errorInfo();
+            throw new ModelException($errors[2]);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Inserting a book in the database
+     */
+    public function insertBook($title, $author, $synopsis, $image, $copies)
+    {
+        $query = $this->pdo->prepare('INSERT INTO livres (titre, auteur, synopsis, image)
+            VALUES (?, ?, ?, ?)');
+        $this->execute($query, array($title, $author, $synopsis, $image));
+
+        
+       $bookId = $this->pdo->lastInsertId();
+        for ($i= 0; $i<$copies; $i++) {
+
+        $query = $this->pdo->prepare('INSERT INTO exemplaires (book_id)
+             VALUES (?)');
+
+         $this->execute($query, array($bookId));
+        }
+
+        // TODO: Créer $copies exemplaires
+    }
+
+    /**
+     * Getting all the books
+     */
+    public function getBooks()
+    {
+        $query = $this->pdo->prepare('SELECT livres.* FROM livres');
+
+        $this->execute($query);
+
+        return $query->fetchAll();
+    }
+
+    /**
+     * Getting one of the books
+     */
+    public function getABook($id)
+    {
+        $query = $this->pdo->prepare('SELECT livres.* FROM livres WHERE livres.id = :id');
+
+        $this->execute($query, array(':id' => $id));
+
+        return $query->fetchAll();
+    }
+
+     /**
+     * Getting exemplaires
+     */
+    public function getExemplairesBooks()
+    {
+        $query = $this->pdo->prepare('SELECT * FROM exemplaires');
+
+        $this->execute($query);
+
+        return $query->fetchAll();
+    }
+
+    /**
+     * Getting exemplaires from a book
+     */
+
+    public function getExemplairesABook($id)
+    {
+        $query = $this->pdo->prepare('SELECT * FROM exemplaires WHERE exemplaires.book_id = :id');
+
+        $this->execute($query, array(':id' => $id));
+
+        return $query->fetchAll();
+    }
+     public function borrowABook($id, $personne, $debut, $fin)
+    {
+        $query = $this->pdo->prepare('INSERT INTO emprunts (id, personne,  debut, fin)
+            VALUES (?, ?, ?, ?)');
+        $this->execute($query, array($id, $personne, $debut, $fin));
+
+
+        return $query->fetchAll();
+    }
+
+
+}
