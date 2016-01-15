bfc914f2092dc4d5d7ebb8dec04c7d4f2fdb7e47
Juliette Rivière <juliette@riviere.im>
2016-01-15 01:15:23 +0100

    gestion d'emprunt

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    // Database
+    'database' => [
+        'engine' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'library',
+        'user' => 'root',
+        'password' => ''
+    ],
+
+    // Administrator auth
+    'admin' => ['admin' => 'password',
+               'admin1' => 'password1',
+               'admin2' => 'password2']
+];
